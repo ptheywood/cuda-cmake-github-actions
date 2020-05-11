716c955b01021075f6a447359e004b282827725e
Peter Heywood <peethwd@gmail.com>
2020-05-11 20:57:30 +0100

    Make it invoke cuda, which will break ci running

--- include/test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-
-// example function from a header.
-
-void helloWorld(){
-    printf("helloWorld\n");
-}
--- src/main.cu
+++ src/main.cu
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#include "test.h"
+#include "test.cuh"
 
 
 int main(int argc, char* argv[]){
