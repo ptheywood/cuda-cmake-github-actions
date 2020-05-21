ea451d38b75706f4237149aff19e7e2871695fbe
Peter Heywood <peethwd@gmail.com>
2020-05-21 20:48:27 +0100

    fix bug in powershell

--- scripts/install_cuda_windows.ps1
+++ scripts/install_cuda_windows.ps1
@@ -45,10 +45,12 @@ $CUDA_PATCH=$Matches.patch
 ## ---------------------------
 # Exit if visual studio is too new for the cuda version.
 $VISUAL_STUDIO_YEAR = $env:visual_studio
+Write-Output "VISUAL_STUDIO_YEAR $($VISUAL_STUDIO_YEAR)"
 if($VISUAL_STUDIO_YEAR.length -ne 0 -and $VISUAL_STUDIO_MIN_CUDA.containsKey($VISUAL_STUDIO_YEAR)){
     $MINIMUM_CUDA_VERSION = $VISUAL_STUDIO_MIN_CUDA[$VISUAL_STUDIO_YEAR]
+    Write-Output "MINIMUM_CUDA_VERSION $($MINIMUM_CUDA_VERSION)"
     if ([version]$CUDA_VERSION_FULL -lt [version]$MINIMUM_CUDA_VERSION) {
-        Write-Output "Error: Visual Studio $($VISUAL_STUDIO_YEAR) requires CUDA >= ${$MINIMUM_CUDA_VERSION}"
+        Write-Output "Error: Visual Studio $($VISUAL_STUDIO_YEAR) requires CUDA >= $($MINIMUM_CUDA_VERSION)"
         exit 1
     }
 } else {
