57a137fa6878bfb8481d21467c08ba7f0ac2aced
Peter Heywood <peethwd@gmail.com>
2020-05-21 19:57:48 +0100

    Actually download cuda again and test nvcc

--- scripts/install_cuda_windows.ps1
+++ scripts/install_cuda_windows.ps1
@@ -1,3 +1,21 @@
+## -------------------
+## Constants
+## -------------------
+
+# Dictionary of known cuda versions and thier download URLS, which do not follow a consistent pattern :(
+$CUDA_KNOWN_URLS = @{
+    "8.0.44" = "http://developer.nvidia.com/compute/cuda/8.0/Prod/network_installers/cuda_8.0.44_win10_network-exe";
+    "8.0.61" = "http://developer.nvidia.com/compute/cuda/8.0/Prod2/network_installers/cuda_8.0.61_win10_network-exe";
+    "9.0.176" = "http://developer.nvidia.com/compute/cuda/9.0/Prod/network_installers/cuda_9.0.176_win10_network-exe";
+    "9.1.85" = "http://developer.nvidia.com/compute/cuda/9.1/Prod/network_installers/cuda_9.1.85_win10_network";
+    "9.2.148" = "http://developer.nvidia.com/compute/cuda/9.2/Prod2/network_installers2/cuda_9.2.148_win10_network";
+    "10.0.130" = "http://developer.nvidia.com/compute/cuda/10.0/Prod/network_installers/cuda_10.0.130_win10_network";
+    "10.1.105" = "http://developer.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.105_win10_network.exe";
+    "10.1.168" = "http://developer.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.168_win10_network.exe";
+    "10.1.243" = "http://developer.download.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.243_win10_network.exe";
+    "10.2.89" = "http://developer.download.nvidia.com/compute/cuda/10.2/Prod/network_installers/cuda_10.2.89_win10_network.exe";
+}
+
 
 ## -------------------
 ## Select CUDA version
@@ -17,80 +35,36 @@ $CUDA_MAJOR=$Matches.major
 $CUDA_MINOR=$Matches.minor
 $CUDA_PATCH=$Matches.patch
 
-
 ## ------------------------------------------------
 ## Select CUDA packages to install from environment
 ## ------------------------------------------------
 
-
+$CUDA_PACKAGES = ""
 $CUDA_PACKAGES_IN = $env:cuda_packages
-echo "$CUDA_PACKAGES_IN"
 # If no packages were provided, error.
 if ($CUDA_PACKAGES_IN.length -eq 0){
-    Write-Output "Please Specify a list of cuda subpackages to install as env:cuda_packages"
+    Write-Output "Please Specify a list of cuda subpackages to install as env:cuda_packages. See https://docs.nvidia.com/cuda/cuda-installation-guide-microsoft-windows/index.html#install-cuda-software"
     exit 1
 }
-
 # Process the list of packages into a list of packages with the version number. Include version specific replacments?
 $SEPARATORS = " ",",",";"
 $package_list = $CUDA_PACKAGES_IN.Split($SEPARATORS, [System.StringSplitOptions]::RemoveEmptyEntries)
-echo $package_list
-
-$CUDA_PACKAGES_STRING=""
 Foreach ($package in $package_list) {
-    echo "$package"
     # Make sure the correct package name is used for nvcc.
     if($package -eq "nvcc" -and [version]$CUDA_VERSION_FULL -lt [version]"9.1"){
         $package="compiler"
     } elseif($package -eq "compiler" -and [version]$CUDA_VERSION_FULL -ge [version]"9.1") {
         $package="nvcc"
     }
-    $CUDA_PACKAGES_STRING += " $($pacakage)_$($CUDA_MAJOR).$($CUDA_MINOR)"
+    $CUDA_PACKAGES += " $($package)_$($CUDA_MAJOR).$($CUDA_MINOR)"
 
 }
-
-echo "CUDA_PACKAGES_STRING"
-echo $CUDA_PACKAGES_STRING
-
-
-
-# Build list of required cuda packages to be installed. 
-# See https://docs.nvidia.com/cuda/cuda-installation-guide-microsoft-windows/index.html#install-cuda-software for pacakge details. 
-
-# @todo - make this configurable outside the script and therefore be fed in? Although it is CUDA version dependent...
-
-# CUDA < 9.1 had a differnt package name for the compiler.
-$NVCC_PACKAGE_NAME="nvcc"
-if ([version]$CUDA_VERSION_FULL -lt [version]"9.1"){
-    $NVCC_PACKAGE_NAME="compiler"
-}
-# Build string containing list of pacakges. Do not need Display.Driver
-$CUDA_PACKAGES  = "$($NVCC_PACKAGE_NAME)_$($CUDA_MAJOR).$($CUDA_MINOR)"
-$CUDA_PACKAGES += " visual_studio_integration_$($CUDA_MAJOR).$($CUDA_MINOR)"
-# $CUDA_PACKAGES += " curand_dev_$($CUDA_MAJOR).$($CUDA_MINOR)"
-# $CUDA_PACKAGES += " nvrtc_dev_$($CUDA_MAJOR).$($CUDA_MINOR)"
-
 
 ## -----------------
 ## Prepare download
 ## -----------------
 
-# Dictionary of known cuda versions and thier download URLS, which do not follow a consistent pattern :(
-$CUDA_KNOWN_URLS = @{
-    "8.0.44" = "http://developer.nvidia.com/compute/cuda/8.0/Prod/network_installers/cuda_8.0.44_win10_network-exe";
-    "8.0.61" = "http://developer.nvidia.com/compute/cuda/8.0/Prod2/network_installers/cuda_8.0.61_win10_network-exe";
-    "9.0.176" = "http://developer.nvidia.com/compute/cuda/9.0/Prod/network_installers/cuda_9.0.176_win10_network-exe";
-    "9.1.85" = "http://developer.nvidia.com/compute/cuda/9.1/Prod/network_installers/cuda_9.1.85_win10_network";
-    "9.2.148" = "http://developer.nvidia.com/compute/cuda/9.2/Prod2/network_installers2/cuda_9.2.148_win10_network";
-    "10.0.130" = "http://developer.nvidia.com/compute/cuda/10.0/Prod/network_installers/cuda_10.0.130_win10_network";
-    "10.1.105" = "http://developer.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.105_win10_network.exe";
-    "10.1.168" = "http://developer.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.168_win10_network.exe";
-    "10.1.243" = "http://developer.download.nvidia.com/compute/cuda/10.1/Prod/network_installers/cuda_10.1.243_win10_network.exe";
-    "10.2.89" = "http://developer.download.nvidia.com/compute/cuda/10.2/Prod/network_installers/cuda_10.2.89_win10_network.exe";
-}
-
-# Build CUDA related variables.
-#  If the specified version is in the known addresses, use that one. 
+# Select the download link if known, otherwise have a guess.
 $CUDA_REPO_PKG_REMOTE=""
 if($CUDA_KNOWN_URLS.containsKey($CUDA_VERSION_FULL)){
     $CUDA_REPO_PKG_REMOTE=$CUDA_KNOWN_URLS[$CUDA_VERSION_FULL]
@@ -107,25 +81,24 @@ $CUDA_REPO_PKG_LOCAL="cuda_$($CUDA_VERSION_FULL)_win10_network.exe"
 ## ------------
 
 # Get CUDA network installer
-# Write-Output "Downloading CUDA Network Installer for $($CUDA_VERSION_FULL) from: $($CUDA_REPO_PKG_REMOTE)"
+Write-Output "Downloading CUDA Network Installer for $($CUDA_VERSION_FULL) from: $($CUDA_REPO_PKG_REMOTE)"
 # Invoke-WebRequest $CUDA_REPO_PKG_REMOTE -OutFile $CUDA_REPO_PKG_LOCAL | Out-Null
-# if(Test-Path -Path $CUDA_REPO_PKG_LOCAL){
-#     Write-Output "Downloading Complete"
-# } else {
-#     Write-Output "Error: Failed to download $($CUDA_REPO_PKG_LOCAL) from $($CUDA_REPO_PKG_REMOTE)"
-#     exit 1
-# }
+if(Test-Path -Path $CUDA_REPO_PKG_LOCAL){
+    Write-Output "Downloading Complete"
+} else {
+    Write-Output "Error: Failed to download $($CUDA_REPO_PKG_LOCAL) from $($CUDA_REPO_PKG_REMOTE)"
+    exit 1
+}
 
 # Invoke silent install of CUDA (via network installer)
-# Write-Output "Installing CUDA $($CUDA_VERSION_FULL) Compiler and Runtime"
-# Start-Process -Wait -FilePath .\"$($CUDA_REPO_PKG_LOCAL)" -ArgumentList "-s $($CUDA_PACKAGES)"
+Write-Output "Installing CUDA $($CUDA_VERSION_FULL). Subpackages $($CUDA_PACKAGES)"
+Start-Process -Wait -FilePath .\"$($CUDA_REPO_PKG_LOCAL)" -ArgumentList "-s $($CUDA_PACKAGES)"
 
-# # Check the return status of the CUDA installer.
-# if ($? -eq $false) {
-#     Write-Output "Error: CUDA installer reported error. $($LASTEXITCODE)"
-#     exit 1 
-# }
-
+# Check the return status of the CUDA installer.
+if (!$?) {
+    Write-Output "Error: CUDA installer reported error. $($LASTEXITCODE)"
+    exit 1 
+}
 
 # Store the CUDA_PATH in the environment for the current session, to be forwarded in the action.
 $CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v$($CUDA_MAJOR).$($CUDA_MINOR)"
@@ -133,6 +106,6 @@ $CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v$($CUDA_MAJOR)
 Write-Output "CUDA_PATH $($CUDA_PATH)"
 $env:CUDA_PATH = "$($CUDA_PATH)"
 
-
-
-Write-Output "Installation Complete!"
+# PATH needs updating elsewhere, anything in here won't persist.
+# Append $CUDA_PATH/bin to path.
+# Set CUDA_PATH as an environmental variable
