a631c378bb6c377e83ad45627e88b12c7cd3fc54
Peter Heywood <peethwd@gmail.com>
2020-05-21 18:08:16 +0100

    try write-output

--- scripts/install_cuda_windows.ps1
+++ scripts/install_cuda_windows.ps1
@@ -25,7 +25,7 @@ $CUDA_KNOWN_URLS = @{
 # Validate CUDA version, extracting components via regex
 $cuda_ver_matched = $CUDA_VERSION_FULL -match "^(?<major>[1-9][0-9]*)\.(?<minor>[0-9]+)\.(?<patch>[0-9]+)$"
 if(-not $cuda_ver_matched){
-    Write-Host "Invalid CUDA version specified, <major>.<minor>.<patch> required. '$CUDA_VERSION_FULL'."
+    Write-Output "Invalid CUDA version specified, <major>.<minor>.<patch> required. '$CUDA_VERSION_FULL'."
     exit 1
 }
 $CUDA_MAJOR=$Matches.major
@@ -40,7 +40,7 @@ if($CUDA_KNOWN_URLS.containsKey($CUDA_VERSION_FULL)){
     $CUDA_REPO_PKG_REMOTE=$CUDA_KNOWN_URLS[$CUDA_VERSION_FULL]
 } else{
     # Guess what the url is given the most recent pattern (at the time of writing, 10.1)
-    Write-Host "note: URL for CUDA ${$CUDA_VERSION_FULL} not known, estimating."
+    Write-Output "note: URL for CUDA ${$CUDA_VERSION_FULL} not known, estimating."
     $CUDA_REPO_PKG_REMOTE="http://developer.download.nvidia.com/compute/cuda/$($CUDA_MAJOR).$($CUDA_MINOR)/Prod/network_installers/cuda_$($CUDA_VERSION_FULL)_win10_network.exe"
 }
 $CUDA_REPO_PKG_LOCAL="cuda_$($CUDA_VERSION_FULL)_win10_network.exe"
@@ -70,22 +70,22 @@ $CUDA_PACKAGES  = "$($NVCC_PACKAGE_NAME)_$($CUDA_MAJOR).$($CUDA_MINOR)"
 ## ------------
 
 # Get CUDA network installer
-Write-Host "Downloading CUDA Network Installer for $($CUDA_VERSION_FULL) from: $($CUDA_REPO_PKG_REMOTE)"
+Write-Output "Downloading CUDA Network Installer for $($CUDA_VERSION_FULL) from: $($CUDA_REPO_PKG_REMOTE)"
 Invoke-WebRequest $CUDA_REPO_PKG_REMOTE -OutFile $CUDA_REPO_PKG_LOCAL | Out-Null
 if(Test-Path -Path $CUDA_REPO_PKG_LOCAL){
-    Write-Host "Downloading Complete"
+    Write-Output "Downloading Complete"
 } else {
-    Write-Host "Error: Failed to download $($CUDA_REPO_PKG_LOCAL) from $($CUDA_REPO_PKG_REMOTE)"
+    Write-Output "Error: Failed to download $($CUDA_REPO_PKG_LOCAL) from $($CUDA_REPO_PKG_REMOTE)"
     exit 1
 }
 
 # Invoke silent install of CUDA (via network installer)
-Write-Host "Installing CUDA $($CUDA_VERSION_FULL) Compiler and Runtime"
+Write-Output "Installing CUDA $($CUDA_VERSION_FULL) Compiler and Runtime"
 Start-Process -Wait -FilePath .\"$($CUDA_REPO_PKG_LOCAL)" -ArgumentList "-s $($CUDA_PACKAGES)"
 
 # Check the return status of the CUDA installer.
 if ($? -eq $false) {
-    Write-Host "Error: CUDA installer reported error. $($LASTEXITCODE)"
+    Write-Output "Error: CUDA installer reported error. $($LASTEXITCODE)"
     exit 1 
 }
 
@@ -96,8 +96,8 @@ if ($? -eq $false) {
 $CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v$($CUDA_MAJOR).$($CUDA_MINOR)"
 $PATH_CUDA_PATH = "$($CUDA_PATH)\bin\;$($CUDA_PATH)\libnvvp\;"
 # Set environmental variables in this session
-Write-Host "CUDA_PATH $($CUDA_PATH)"
-Write-Host "Setting CUDA_PATH and PATH."
+Write-Output "CUDA_PATH $($CUDA_PATH)"
+Write-Output "Setting CUDA_PATH and PATH."
 $env:CUDA_PATH = "$($CUDA_PATH)"
 $env:PATH = "$($PATH_CUDA_PATH)$($env:PATH)"
 # Make the new values persist the reboot via the registry.
@@ -107,10 +107,10 @@ $env:PATH = "$($PATH_CUDA_PATH)$($env:PATH)"
 
 nvcc -V
 
-Write-host "This should be adding to the path? and env? ${$CUDA_PATH}"
+Write-Output "This should be adding to the path? and env? ${$CUDA_PATH}"
 # Add to github actions.
-Write-Host "::add-path::${$CUDA_PATH}\bin"
-Write-Host "::add-path::${$CUDA_PATH}\libnvvp"
-Write-Host "::set-env name=CUDA_PATH::${$CUDA_PATH}"
+Write-Output "::add-path::${$CUDA_PATH}\bin"
+Write-Output "::add-path::${$CUDA_PATH}\libnvvp"
+Write-Output "::set-env name=CUDA_PATH::${$CUDA_PATH}"
 
-Write-Host "Installation Complete!"
+Write-Output "Installation Complete!"
